b4aeb50dd427786072ccbc5f6f779c444b893cca
Osrosario <114501083+Osrosario@users.noreply.github.com>
2023-02-22 12:33:34 -0500

    Added Animal Printlns

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class AnimalPicker
 {
+    private static Scanner userInput = new Scanner(System.in);
+    
     public static void main(String[] args) 
     {
-        
+        System.out.println("\nWelcome to Animal Picker!");
+        System.out.println("\nChoose enter an animal you wish to see:");
+        System.out.println("- Dog");
+        System.out.println("- Cat");
+
+        System.out.print("\nI want to see a: ");
+        String animal = userInput.nextLine();
+
+        if (animal.equalsIgnoreCase("Dog"))
+        {
+            System.out.println("\nDOG ASCII ART HERE");
+        }
+        else if (animal.equalsIgnoreCase("Cat"))
+        {
+            System.out.println("\nCAT ASCII ART HERE");
+        }
     }
 }
